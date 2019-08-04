37fab1b66b10806db0ee0b086ca689fbac0e6cb2
Bert Roex <>
2019-08-04 12:28:00 +0200

    Comment out scala

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,7 +34,7 @@ source ${MY_DIR}/configuration-osx.sh "$fullname" $emailaddress "$phonenumber"
 source ${MY_DIR}/configuration-terminal.sh
 source ${MY_DIR}/git.sh "$fullname" $emailaddress
 source ${MY_DIR}/java.sh
-source ${MY_DIR}/scala.sh
+# source ${MY_DIR}/scala.sh
 source ${MY_DIR}/android.sh
 source ${MY_DIR}/js.sh
 source ${MY_DIR}/cli.sh
