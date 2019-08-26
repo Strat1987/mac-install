0e9325aa21b544fcc7ee6f93937a90d4d54eb335
Bert Roex <>
2019-08-26 14:42:47 +0200

    save credentials to file for re runs of the install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,15 +13,27 @@ clear
 
 echo "Hello, "$USER".  This script will install your mac. It asks for your email address and phone number in order to customize the Git config and the lost&found message on the login screen"
 
-echo
-echo "Enter your full name and press [ENTER]"
-read fullname
-echo
-echo "Enter your email address and press [ENTER]"
-read emailaddress
-echo "Enter your phone number and press [ENTER]"
-read phonenumber
+echo "Reading config from credentials.cfg ...."
+source ./credentials.cfg > /dev/null 2>&1
 
+# Prompt for TST Credentials (in case they are not retrieved from the credentials_tst.cfg file)
+if [[ -z "$fullname" ]]; then
+    read -p "Enter your full name and press [ENTER]" fullname
+fi
+if [[ -z "$emailaddress" ]]; then
+    read -s -p "Enter your e-mail address and press [ENTER] " emailaddress
+fi
+if [[ -z "$phonenumber" ]]; then
+    read -s -p "Enter your phone number and press [ENTER] " phonenumber
+fi
+
+echo "fullname='$fullname'" > credentials.cfg
+echo "emailaddress='$emailaddress'" >> credentials.cfg
+echo "phonenumber='$phonenumber'" >> credentials.cfg
+
+echo "Your full name: $fullname"
+echo "Your e-mail address: $emailaddress"
+echo "Your Phone number: $phonenumber"
 
 echo
 echo "Setting up machine..."
