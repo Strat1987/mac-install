43bc20394b4031f1a7ae5b0c69c7a35b2dcfbf52
Bert Roex <bert.roex@xt-i.com>
2020-08-29 13:21:33 +0200

    Add TODO to check if I should switch out antigen for custom zsh plugin management

--- scripts/js.sh
+++ scripts/js.sh
@@ -2,5 +2,5 @@ echo
 echo "Installing Javascript tools"
 # Prefer zsh plugin over brew install to install Node https://github.com/nvm-sh/nvm
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-
+# TODO check if using antigen https://antigen.sharats.me/ is a better approach for loading zsh plugins
 brew install yarn
