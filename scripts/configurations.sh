2a74a4bc62ecdbb103821ea89ea1267a0315348e
Bert Roex <bert.roex@xt-i.com>
2021-10-27 17:09:42 +0200

    Update loginitems

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -3,6 +3,7 @@ echo
 
 # OS X Sierra prevents changes to TCC.db, so only enable tccutil when SIP is disabled
 #brew install tccutil
+
 # add loginitems repo https://github.com/OJFord/loginitems
 brew tap OJFord/formulae
 brew install loginitems
@@ -10,18 +11,44 @@ brew install loginitems
 echo "Configuring iTerm"
 cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
-echo "Configuring Spectacle"
+echo "Configuring ShiftIt"
 #sudo tccutil --insert "com.divisiblebyzero.Spectacle" # Enable Accessibility Settings
-loginitems -a "Spectacle" -p "/Applications/Spectacle.app" # Start on login
-open /Applications/Spectacle.app
-echo "Installing Spectacle shortcuts"
-#cp files/Shortcuts.json ~/"Library/Application Support/Spectacle/Shortcuts.json"
-ditto files/Shortcuts.json ~/"Library/Application Support/Spectacle/Shortcuts.json"
+loginitems -a "ShiftIt" -p "/Applications/ShiftIt.app/" # Start at login
+open /Applications/ShiftIt.app
 
-echo "Configuring FlyCut"
-loginitems -a "Flycut" -p "/Applications/Flycut.app" # Start at login
-open /Applications/Flycut.app
+echo "Configuring Amphetamine"
+loginitems -a "Amphetamine" -p "/Applications/Amphetamine.app/" # Start at login
+open /Applications/Amphetamine.app/
 
+echo "Configuring Franz"
+loginitems -a "Franz" -p "/Applications/Franz.app/" # Start at login
+open /Applications/Franz.app/
+
+echo "Configuring Flux"
+loginitems -a "Flux" -p "/Applications/Flux.app/" # Start at login
+open /Applications/Flux.app/
+
+echo "Configuring BeardedSpice"
+loginitems -a "BeardedSpice" -p "/Applications/BeardedSpice.app/" # Start at login
+open /Applications/Flux.app/
+
+echo "Configuring SpotMenu"
+loginitems -a "SpotMenu" -p "/Applications/SpotMenu.app/" # Start at login
+open /Applications/Flux.app/
+
+echo "Configuring Google Drive"
+loginitems -a "Google Drive" -p "/Applications/Google\ Drive.app/" # Start at login
+open /Applications/Google\ Drive.app/
+
+echo "Configuring CopyClip 2"
+loginitems -a "CopyClip 2" -p "/Applications/CopyClip\ 2.app/" # Start at login
+open /Applications/CopyClip\ 2.app/
+
+echo "Configuring CheatSheet"
+loginitems -a "CheatSheet" -p "/Applications/CheatSheet.app/" # Start at login
+open /Applications/Flux.app/
+
+#https://github.com/sindresorhus/quick-look-plugins
 echo "Installing QuickLook extensions"
 brew install qlstephen  # QLStephen is a QuickLook plugin to view plain text files without a file extension
 #brew install webpquicklook # WebP
