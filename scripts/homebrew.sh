0a65fb09005d4935202eb4937abadd4de10b3b02
Bert Roex <bert.roex@xt-i.com>
2021-10-28 11:48:22 +0200

    update homebrew install

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -4,24 +4,36 @@ if hash brew 2>/dev/null; then
   echo "Homebrew is already installed!"
 else
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo
-echo "Ensuring you have the latest Homebrew..."
-brew update
+  echo
+  echo "Ensuring your Homebrew directory is writable..."
+  sudo chown -R $(whoami) /usr/local/bin
+
+  echo
+  echo "Adding Homebrew's sbin to your PATH..."
+  echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bashrc
+  echo "if [ -f ~/.bashrc ]; then source ~/.bashrc fi" >> ~/.bash_profile
+
+  currentDir=${pwd}
+  cd ~
+  . .bash_profile
+  . .bashrc
+  cd $currentDir
+
+  echo
+  echo "Installing Homebrew services..."
+  brew tap homebrew/services
+`
+fi
 
 echo
 echo "Ensuring you have a healthy Homebrew environment..."
 brew doctor
 
 echo
-echo "Ensuring your Homebrew directory is writable..."
-sudo chown -R $(whoami) /usr/local/bin
-
-echo
-echo "Installing Homebrew services..."
-brew tap homebrew/services
+echo "Ensuring you have the latest Homebrew..."
+brew update
 
 echo
 echo "Upgrading existing brews..."
@@ -31,12 +43,5 @@ echo "Cleaning up your Homebrew installation..."
 brew cleanup
 
 echo
-echo "Adding Homebrew's sbin to your PATH..."
-echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bashrc
-echo "if [ -f ~/.bashrc ]; then source ~/.bashrc fi" >> ~/.bash_profile
-
-currentDir=${pwd}
-cd ~
-. .bash_profile
-. .bashrc
-cd $currentDir
+echo "Ensuring your Homebrew environment is still healthy..."
+brew doctor
