10c2172649c8accda1da47d62f44fb30007882bd
Bert Roex <roexbert@gmail.com>
2026-09-19 16:10:48 +0200

    adding brew trust for heroku and confluent

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -11,6 +11,7 @@ brew install google-cloud-sdk
 
 # Heroku CLI
 brew tap heroku/brew
+brew trust --formula heroku/brew/heroku
 brew install heroku
 # Firebase Command Line Interface
 #npm -g install firebase-tools
@@ -19,6 +20,8 @@ brew install heroku
 brew install awscli
 brew install awsebcli
 
+brew tap confluent/tap/cli
+brew trust --formula confluent/tap/cli/confluent-cli
 brew install --cask confluent-cli
 
 brew install kubernetes-cli
