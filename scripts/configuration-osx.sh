3c571301c2fb96d051fda7613c5e484c180bbb5c
Bert Roex <roexbert@gmail.com>
2026-09-20 09:41:42 +0200

    customize dock size

--- scripts/configuration-osx.sh
+++ scripts/configuration-osx.sh
@@ -141,8 +141,8 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Desktop and Dock
 ###############################################################################
 defaults write com.apple.dock orientation -string "left"
-defaults write com.apple.dock tilesize -integer 24
-defaults write com.apple.dock largesize -integer 40
+defaults write com.apple.dock tilesize -integer 42
+defaults write com.apple.dock largesize -integer 63
 defaults write com.apple.dock minimize-to-application -bool true
 killall Dock
 
