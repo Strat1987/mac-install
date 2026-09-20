33b56435a585a0efa54601fadea319a27fc6e0f5
Bert Roex <roexbert@gmail.com>
2026-09-20 12:11:34 +0200

    add trust for entire tap justinmayer/tap

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -151,7 +151,7 @@ brew install handbrake
 # Homebrew removed formula options as of Homebrew version 2.0
 # so no more using the core ffmpeg formula
 brew tap justinmayer/tap
-brew trust --formula justinmayer/tap/ffmpeg
+brew trust justinmayer/tap
 brew install justinmayer/tap/ffmpeg --with-chromaprint --with-fdk-aac
 # includes support WebM: ffmpeg -i video.mp4 -strict -2 video.webm
 brew install iina
