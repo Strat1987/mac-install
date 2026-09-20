77ff5d6922d663504c0a04f85f682fcbf9a1b81d
Bert Roex <roexbert@gmail.com>
2026-09-20 20:37:23 +0200

    configurations.sh update application list

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -7,14 +7,6 @@ brew install loginitems
 echo "Configuring iTerm"
 cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
-echo "Configuring ShiftIt"
-loginitems -a "ShiftIt" -p "/Applications/ShiftIt.app/" # Start at login
-open /Applications/ShiftIt.app
-
-echo "Configuring Amphetamine"
-loginitems -a "Amphetamine" -p "/Applications/Amphetamine.app/" # Start at login
-open /Applications/Amphetamine.app/
-
 echo "Configuring Franz"
 loginitems -a "Franz" -p "/Applications/Franz.app/" # Start at login
 open /Applications/Franz.app/
@@ -23,13 +15,13 @@ echo "Configuring Flux"
 loginitems -a "Flux" -p "/Applications/Flux.app/" # Start at login
 open /Applications/Flux.app/
 
-echo "Configuring BeardedSpice"
-loginitems -a "BeardedSpice" -p "/Applications/BeardedSpice.app/" # Start at login
-open /Applications/Flux.app/
+echo "Configuring Beardie"
+loginitems -a "Beardie" -p "/Applications/Beardie.app/" # Start at login
+open /Applications/Beardie.app/
 
 echo "Configuring SpotMenu"
 loginitems -a "SpotMenu" -p "/Applications/SpotMenu.app/" # Start at login
-open /Applications/Flux.app/
+open /Applications/SpotMenu.app/
 
 echo "Configuring Google Drive"
 loginitems -a "Google Drive" -p "/Applications/Google\ Drive.app/" # Start at login
@@ -38,10 +30,6 @@ open /Applications/Google\ Drive.app/
 echo "Configuring CopyClip 2"
 loginitems -a "CopyClip 2" -p "/Applications/CopyClip\ 2.app/" # Start at login
 open /Applications/CopyClip\ 2.app/
-
-echo "Configuring CheatSheet"
-loginitems -a "CheatSheet" -p "/Applications/CheatSheet.app/" # Start at login
-open /Applications/Flux.app/
 
 #https://github.com/sindresorhus/quick-look-plugins
 echo "Installing QuickLook extensions"
