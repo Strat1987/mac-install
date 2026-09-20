047a8e2f2a8631bd4d97408490caa3a5ca4f2ac1
Bert Roex <roexbert@gmail.com>
2026-09-20 10:01:10 +0200

    update /etc/hosts utility switchhosts

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -39,7 +39,7 @@ brew install radix
 brew install neodisk
 
 brew install coconutbattery # Battery check
-brew install hostfileapp # custom HOSTS files that can be dynamically switched
+brew install switchhosts # custom HOSTS files that can be dynamically switched
 brew install hostess
 brew install keybase # control Mac Media Keys
 brew install calibre # e-book app
